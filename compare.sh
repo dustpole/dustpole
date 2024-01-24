ebd96247becd2186766756863796f068641677a6
Dustin Pollreis <dustpole@gmail.com>
2024-01-23 22:38:07 -0600

    Update compare.sh

--- compare.sh
+++ compare.sh
@@ -2,5 +2,5 @@ for i in \
     "/etc/postfix/master.cf" \
     "/etc/postfix/ad_virtual_mailbox_maps.cf" \
 do
-   diff <(cat %i) <(cut -f2 /opt/bak/%i)
+   diff <(cat $i) <(cut -f2 /opt/bak/$i)
 done

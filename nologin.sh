2bb1b24afaf573d42652a5ad1a1da34b4e305dfa
Dustin Pollreis <dustpole@gmail.com>
2024-01-25 20:23:30 -0600

    Update nologin.sh

--- nologin.sh
+++ nologin.sh
@@ -3,6 +3,6 @@
 while IFS=: read -r username _ uid _; do
     if [ "$uid" -ge 5000 ]; then #if uid is greater than or equal to 5000
         usermod $username -s /sbin/nologin;
-    	printf "$username, user set to no login";
+    	printf "$username, user set to no login \n";
     fi
 done < /etc/passwd
